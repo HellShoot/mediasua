8f29d2709accf094077a21abceb8d8339873036c
Rafael Ferreira <mail@rafaelferreira.pt>
2016-02-10 11:20:22 +0000

    requirements

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='Example-CherryPy',
       author='Eric Rich',
       author_email='sferich888@gamil.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['CherryPy>=3.2.4'],
+      install_requires=['CherryPy>=3.2.4', 'lxml', 'requests', 'bs4'],
      )
--- wsgi/application.py
+++ wsgi/application.py
@@ -39,5 +39,60 @@ class Root(object):
     def calcular(self, username, password):
         return username + password
 
+
+## medias
+class Cadeira:
+    def __init__(self, codigo, ano, semestre, nota, nome, ects):
+        self.codigo = codigo
+        self.nota = nota
+        self.nome = nome
+        self.ects = ects
+        self.ano = ano
+        self.semestre = semestre
+
+
+def notas(username, password):
+    s = requests.Session()
+    r = s.get(url="https://paco.ua.pt/secvirtual/c_historiconotas.asp")
+
+    tree = html.fromstring(r.content)
+    o = urlparse(r.url)
+
+    url_auth = o.scheme + "://" + o.netloc + tree.body.forms[0].attrib["action"]
+    r = s.post(url_auth, data={'j_password': password, 'j_username': username, 'Submeter': 'OK'})
+
+    tree = html.fromstring(r.content)
+
+    inputs = tree.xpath("//input")
+
+    payload = {'RelayState': inputs[0].value, 'SAMLResponse': inputs[1].value}
+
+    r = s.post(tree.body.forms[0].attrib["action"], data=payload)
+    r = s.get("https://paco.ua.pt/secvirtual/c_planocurr.asp")
+
+    soup = BeautifulSoup(r.content)
+    table = soup.find('table', attrs={'width': '95%', 'align': 'center', 'cellspadding': '2'})
+
+    cadeiras = []
+
+    for row in table.findAll("tr"):
+        cells = row.findAll("td")
+        if len(cells) == 8 and cells[1].text.rstrip().replace("\r\n\t", "") != 'Codigo':
+            if len(cells[7].text.rstrip().replace("\r\n\t", "")) != 0:
+                cadeiras += [Cadeira(codigo=int(cells[1].text.rstrip().replace("\r\n\t", "")),
+                                     nome=cells[2].text.rstrip().replace("\r\n\t", ""),
+                                     ano=int(cells[3].text.rstrip().replace("\r\n\t", "")),
+                                     semestre=int(cells[4].text.rstrip().replace("\r\n\t", "")),
+                                     ects=float(cells[6].text.rstrip().replace("\r\n\t", "").replace(",", ".")),
+                                     nota=float(cells[7].text.rstrip().replace("\r\n\t", "").replace(",", ".")))]
+    nota = 0.0
+    creditos = 0.0
+
+    for cadeira in cadeiras:
+        nota += (cadeira.nota * cadeira.ects)
+        creditos += cadeira.ects
+
+    nota = nota / creditos
+
 application = cherrypy.Application(Root(), script_name=None, config=None)
 
