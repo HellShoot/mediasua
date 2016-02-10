a981095329ccda5e82f767c50ca6ee18f01fed34
Rafael Ferreira <mail@rafaelferreira.pt>
2016-02-10 10:48:39 +0000

    teste

--- wsgi/application.py
+++ wsgi/application.py
@@ -12,9 +12,20 @@ if cherrypy.__version__.startswith('3.0') and cherrypy.engine.state == 0:
     cherrypy.engine.start(blocking=False)
     atexit.register(cherrypy.engine.stop)
 
+
+# Path to static directory
+repo = os.environ['OPENSHIFT_REPO_DIR']
+STATIC_DIR = os.path.join(repo, u"wsgi", u"static")
+
 class Root(object):
+
+    _cp_config = {'tools.staticdir.on': True,
+                  'tools.staticdir.dir': STATIC_DIR,
+                  'tools.staticdir.index': 'index.html',
+    }
+
     def index(self):
-        return open("./static/index.html", "r").read()
+        return open(os.path.join(STATIC_DIR, u'index.html'))
     index.exposed = True
 
 application = cherrypy.Application(Root(), script_name=None, config=None)
