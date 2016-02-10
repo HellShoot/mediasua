b6c7c5daeb71d48a90dbcbe91b20d9cc735e7b4c
Rafael Ferreira <mail@rafaelferreira.pt>
2016-02-10 12:48:34 +0000

    console

--- wsgi/static/js/media.js
+++ wsgi/static/js/media.js
@@ -2,9 +2,7 @@ $(document).ready(function(){
    $("#calcular").click(function(){
        $.post( "/calcular", { username: $("#username").val(), password: $("#password").val() })
             .done(function( data ) {
-                alert( "Data Loaded: " + data );
-            });
-
-       console.log("calculando!");
+               console.log(data);
+            });;
    });
 });
